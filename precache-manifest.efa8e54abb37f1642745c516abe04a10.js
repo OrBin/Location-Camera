fd3ea56ba43c1bdf7723f9bf8e85a51463d06d52
OrBin <orbin50@gmail.com>
2020-07-03 10:47:11 +0000

    Deploying to gh-pages from  @ 6947ab777ff06c22d85874e3513d21c453f29ea3 🚀

---
+++ precache-manifest.efa8e54abb37f1642745c516abe04a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4af6dc7d74a2d1e18f241af8db1dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c8a5d0d81d71a694234",
+    "url": "/static/css/main.efe03bf9.chunk.css"
+  },
+  {
+    "revision": "a606a9aa2fed30652590",
+    "url": "/static/js/2.cf5fa86a.chunk.js"
+  },
+  {
+    "revision": "881b4cd60baf4ebc476fd3c43b040b40",
+    "url": "/static/js/2.cf5fa86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8a5d0d81d71a694234",
+    "url": "/static/js/main.e4060ff8.chunk.js"
+  },
+  {
+    "revision": "9855cac1f77739d14de9",
+    "url": "/static/js/runtime-main.8357af5e.js"
+  }
+]);
